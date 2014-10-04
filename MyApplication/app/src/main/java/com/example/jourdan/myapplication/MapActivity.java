f984dd25f7849c63e332e21c0ed36add2f6b9daf
westjour <westjour@msu.edu>
2014-10-04 09:11:58 -0400

    fixed action bar in Map Activity

--- MyApplication/app/src/main/java/com/example/jourdan/myapplication/MapActivity.java
+++ MyApplication/app/src/main/java/com/example/jourdan/myapplication/MapActivity.java
@@ -43,13 +43,6 @@ public class MapActivity extends BaseActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.map, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
