d049b34d7d6f77a02a97f24b2516ed0b0ad89264
westjour <westjour@msu.edu>
2014-10-04 08:54:30 -0400

    detect user current GPS location

--- MyApplication/app/src/main/java/com/example/jourdan/myapplication/HomeActivity.java
+++ MyApplication/app/src/main/java/com/example/jourdan/myapplication/HomeActivity.java
@@ -1,18 +1,25 @@
 package com.example.jourdan.myapplication;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Intent;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
-import android.content.Intent;
 import android.widget.EditText;
 
-public class MyActivity extends BaseActivity {
+public class HomeActivity extends BaseActivity {
     public final static String SALARY = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_home);
+
+        // Get current GPS location
+        LocationManager locationManager = (LocationManager)
+        getSystemService(this.LOCATION_SERVICE);
+        LocationListener locationListener = new MyLocationListener(this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
     }
 
     /** Called when the user clicks the Send button */
--- MyApplication/app/src/main/java/com/example/jourdan/myapplication/MapActivity.java
+++ MyApplication/app/src/main/java/com/example/jourdan/myapplication/MapActivity.java
@@ -1,25 +1,17 @@
 package com.example.jourdan.myapplication;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import android.util.Log;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MapActivity extends BaseActivity {
 
