eb0d6201f8cf8cd73a05155f632be2a87f71a6b6
Tim Sloncz <tsloncz@gmail.com>
2014-10-04 18:10:34 -0400

    Added checkBox support to HomePage and AppData

--- MyApplication/app/src/main/java/com/example/jourdan/myapplication/HomeActivity.java
+++ MyApplication/app/src/main/java/com/example/jourdan/myapplication/HomeActivity.java
@@ -5,6 +5,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -22,8 +23,10 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Tesing
         Utility util = new Utility();
-        util.getAffordableAssistedHousing("GrandRapids");
+        //util.getHistoricPlaces("Lansing");
+
         setContentView(R.layout.activity_home);
 
         // Get current GPS location
@@ -37,10 +40,60 @@ public class HomeActivity extends BaseActivity {
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
     }
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkbox_parks:
+                if (checked)
+                {
+                    //Add to attributes in appData
+                    appData.addAttribute("parks");
+                }
+                else
+                {
+                    //Remove from attributes in appData
+                    appData.removeAttribute("parks");
+
+                }
+                break;
+            case R.id.checkbox_schools:
+                if (checked)
+                {
+                    appData.addAttribute("schools");
+                }
+                else
+                {
+                    appData.removeAttribute("schools");
+                }
+                break;
+            case R.id.checkbox_Tax:
+                if (checked)
+                {
+                    appData.addAttribute("tax");
+                }
+                else
+                {
+                    appData.removeAttribute("tax");
+                }
+                break;
+            case R.id.checkbox_historic:
+                if (checked)
+                {
+                    appData.addAttribute("historic");
+                }
+                else
+                {
+                    appData.removeAttribute("historic");
+                }
+                break;
+        }
+        Log.d(TAG, "Attributes: " + appData.getAttributes());
+    }
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) throws JSONException {
-        //JSONObject appData = new JSONObject();
         JSONObject location = new JSONObject();
         JSONArray cities = new JSONArray();
 
@@ -48,16 +101,14 @@ public class HomeActivity extends BaseActivity {
         Intent intent = new Intent(this, MapActivity.class);
         String lat = Double.toString( locationListener.mLat );
         location.put("lat",lat);
-        String lng = Double.toString( locationListener.mLng );
+        String lng = Double.toString(locationListener.mLng);
         location.put("lng",lng);
         cities.put(location);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String salary = editText.getText().toString();
         appData.setSalary(salary);
         appData.addCity(location);
-        //appData.put("salary", salary);
-        //appData.put("cities", cities);
-        //intent.putExtra(APP_DATA, appData.toString());
+
         ((MyApplication) this.getApplication()).setAppData(appData);
         startActivity(intent);
     }
