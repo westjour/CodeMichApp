69a491e98533a7c6b64e6d66327c440a9ffca7fc
Tim Sloncz <tsloncz@gmail.com>
2014-10-03 20:49:16 -0400

    Added map support and map view to MapActivity
    Need google play SDK
    SHA-1 fingerprint needs to be registered with google api to connect

--- MyApplication/app/src/main/java/com/example/jourdan/myapplication/MapActivity.java
+++ MyApplication/app/src/main/java/com/example/jourdan/myapplication/MapActivity.java
@@ -4,8 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.content.Intent;
+import android.widget.Toast;
+import android.app.Activity;
 
 
 public class MapActivity extends Activity {
@@ -13,19 +28,11 @@ public class MapActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // URL format for send lat lng to google geocode api to get location information
+        //https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=API_KEY
+        //GET("url");
+        setContentView(R.layout.activity_map);
 
-        // Get the message from the intent
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MyActivity.SALARY);
-
-        // Create the text view
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
-
-        // Set the text view as the activity layout
-        setContentView(textView);
-        //setContentView(R.layout.activity_map);
     }
 
 
@@ -46,5 +53,46 @@ public class MapActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    public static String GET(String url){
+        System.out.println("Getting URL DATA");
+        url = "http://data.michigan.gov/resource/kvss-tqw8.json";
+
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 }
