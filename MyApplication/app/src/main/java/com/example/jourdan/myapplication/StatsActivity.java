ce6544d7141b89e5bb6ab7a15b873b3cc3f5fa05
Tim Sloncz <tsloncz@gmail.com>
2014-10-04 16:14:53 -0400

    Added methods to get more MiData. And get cityName from coordinates method to utillity

--- MyApplication/app/src/main/java/com/example/jourdan/myapplication/StatsActivity.java
+++ MyApplication/app/src/main/java/com/example/jourdan/myapplication/StatsActivity.java
@@ -2,17 +2,22 @@ package com.example.jourdan.myapplication;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
 
 public class StatsActivity extends BaseActivity {
     final String TAG = "StatsActivity.java";
     private AppData appData = null;
+    private Utility util = new Utility();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         appData = ((MyApplication) this.getApplication()).getAppData();
         Log.d(TAG, "AppData: "+appData.getSalaryString() +" "+ appData.getCities());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stats);
         Log.d(TAG, TAG + " created successfully" );
     }
 }
